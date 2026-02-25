61746ccc7a941988c8e48460483dbb5e42f40f06
Uciel Sola <solauciel@gmail.com>
2026-02-25 19:26:09 -0300

    feat: Implement WXT + Svelte 5 + Tailwind 4 Chrome Extension
    - Switch from SvelteKit to WXT framework
    - Add Svelte 5 with runes support
    - Add Tailwind CSS 4 + DaisyUI
    - Create proper entrypoints (popup, options, background, content)
    - Add justfile for task running
    - Update README with new tech stack

---
+++ src/entrypoints/background.ts
@@ -0,0 +1,3 @@
+export default defineBackground(() => {
+  console.log('Excalivault background script loaded');
+});
---
+++ src/entrypoints/options/options.ts
@@ -0,0 +1,7 @@
+import '../../app.css';
+import Options from './Options.svelte';
+import { mount } from 'svelte';
+
+mount(Options, {
+  target: document.getElementById('app')!
+})
